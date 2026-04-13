fb50338daee29fb55f8679dd51c6aab5bc99726f
nicolascanas <ig.contato.nico@gmail.com>
2026-04-13 17:54:15 -0300

    feat: add initial home screen with game mode selection

---
+++ script.js
@@ -0,0 +1,10 @@
+const vsComputerBtn = document.getElementById("vs-computer");
+const vsPlayerBtn = document.getElementById("vs-player");
+
+vsComputerBtn.addEventListener("click", () => {
+  console.log("Mode: vs computer");
+});
+
+vsPlayerBtn.addEventListener("click", () => {
+  console.log("Mode: 2 players");
+});
